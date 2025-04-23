f6c79e23a9a4482a99730d33ad5b602d3c97b64d
kj-im <kjim@suprema.co.kr>
2025-04-23 17:00:27 +0900

    feat: DLtable 컴포넌트의 스타일 및 구조 개선

--- src/components/DLtable/index.js
+++ src/components/DLtable/index.js
@@ -2,16 +2,16 @@ import Cmd from '@site/src/components/Cmd';
 import clsx from 'clsx';
 import styles from './styles.module.css';
 
-export function DLtable({ type, children, className }) {
+export function DLtable({ type, children }) {
   return (
-    <div className={clsx(styles.dltable, 'overflow-x', )}>
-      <DLHead type={type} />
+    <div className={clsx(styles.dltable, styles[type], 'overflow-x', )}>
+      <DLHead />
       {children}
     </div>
   )
 }
 
-function DLHead({ type }) {
+function DLHead() {
   return (
     <div className={styles.dlhead}>
       <div className={styles.dlheaditem}>항목</div>
