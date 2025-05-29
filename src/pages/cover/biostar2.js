187e436ab35c7f183826483472cd55b0d9ef36dd
kj-im <kjim@suprema.co.kr>
2025-05-30 08:14:22 +0900

    버전 번호를 v2.9.8에서 v2.9.9로 업데이트하고, 테마 문서의 내비게이션 표시를 숨김 처리함

--- src/pages/cover/biostar2.js
+++ src/pages/cover/biostar2.js
@@ -10,7 +10,7 @@ export default function Cover() {
     const { i18n: { currentLocale, label }, siteConfig } = useDocusaurusContext();
     const title = "BioStar 2";
     const subtitle = "Revision Notes";
-    const ver = "v2.9.8";
+    const ver = "v2.9.9";
     const lang = siteConfig.i18n.localeConfigs[currentLocale].label;
     const langLabel = currentLocale.toUpperCase();
     const num = `${langLabel} 301.00.BS2`;
